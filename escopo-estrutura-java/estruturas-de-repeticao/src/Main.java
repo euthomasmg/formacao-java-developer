9b44b33c688d624e44e8b33142c63584943f4cf3
Thomas <thomas2003.mayer@gmail.com>
2024-12-09 21:25:20 -0300

    .

--- escopo-estrutura-java/estruturas-de-repeticao/src/Main.java
+++ escopo-estrutura-java/estruturas-de-repeticao/src/Main.java
@@ -2,6 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        int i;
+
+        for ( i = 1; i <= 20; i++){
+
+            System.out.println(i);
+        }
+
+        System.out.println("Contou até " + i);
+
+
     }
 }
