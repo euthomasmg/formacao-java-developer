2fcab2fda661a44c0bdf8666c345ee10d11bf670
Thomas <thomas2003.mayer@gmail.com>
2024-12-27 17:58:08 -0300

    new commit

--- escopo-estrutura-java/estruturas-de-repeticao/src/BreackContinue.java
+++ escopo-estrutura-java/estruturas-de-repeticao/src/BreackContinue.java
@@ -2,10 +2,22 @@ public class BreackContinue {
 
     public static void main (String[] args) {
 
+        //o brank para o for quando o if e a condição dele for verdadeira.
+
         for (int i = 1; i <= 5; i++){
 
             if (i == 3){
                 break;
+            }
+            System.out.println(i);
+        }
+
+        //quando usar o continue, ele vai pular um numero do contador, nesse caso.
+
+        for (int i = 1; i <= 5; i++){
+
+            if (i == 3){
+                continue;
             }
             System.out.println(i);
         }
