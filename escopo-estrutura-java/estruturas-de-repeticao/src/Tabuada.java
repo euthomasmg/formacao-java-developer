2497efdaf472ea6777f6c2c36a6e7243033d2780
Thomas <thomas2003.mayer@gmail.com>
2024-12-28 01:35:55 -0300

    numeros pares

--- escopo-estrutura-java/estruturas-de-repeticao/src/Tabuada.java
+++ escopo-estrutura-java/estruturas-de-repeticao/src/Tabuada.java
@@ -6,7 +6,15 @@ public class Tabuada {
 
         Scanner read = new Scanner(System.in);
 
+        int n1;
 
+        System.out.print("Informe um número inteiro: ");
+        n1 = read.nextInt();
+
+        for(int i = 1; i <= 10; i++){
+
+            System.out.println(n1 + " x " + i + " = " + n1 * i);
+        }
 
     }
 }
