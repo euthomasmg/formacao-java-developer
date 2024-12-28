3263e7fee78d6ece55cf8914c0db123781052c31
Thomas <thomas2003.mayer@gmail.com>
2024-12-28 15:55:21 -0300

    even numbers exercise

--- escopo-estrutura-java/estruturas-de-repeticao/src/NumerosPares.java
+++ escopo-estrutura-java/estruturas-de-repeticao/src/NumerosPares.java
@@ -6,6 +6,15 @@ public class NumerosPares {
 
         Scanner read = new Scanner(System.in);
 
+        int resultado, soma = 0;
+
+        for(int i = 1; i <= 50; i++){
+
+            resultado = i * 2;
+
+            soma += resultado;
+        }
+        System.out.println("A soma dos números pares de 0 a 100 é: " + soma);
 
     }
 }
