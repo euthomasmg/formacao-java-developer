4ed9aa4922ccb6aab1628ac1568371f3c891fb78
Thomas <thomas2003.mayer@gmail.com>
2024-12-27 23:42:31 -0300

    .

---
+++ escopo-estrutura-java/estruturas-de-repeticao/src/ExemploForArray.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ExemploForArray {
+
+    public static void main (String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        String alunos[] = {"Thomas", "Gabby"};
+
+        for (int i = 0; i < alunos.length; i++)
+        {
+
+            System.out.println("O aluno no indice " + i + " é " + alunos[i]);
+        }
+
+
+    }
+}
