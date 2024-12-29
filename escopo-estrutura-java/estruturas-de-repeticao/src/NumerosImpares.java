fd930e06608d8392b92285ca59ce7e02ea46be20
Thomas <thomas2003.mayer@gmail.com>
2024-12-29 17:37:47 -0300

    numeros impares

---
+++ escopo-estrutura-java/estruturas-de-repeticao/src/NumerosImpares.java
@@ -0,0 +1,15 @@
+public class NumerosImpares {
+
+    public static void main(String[] args){
+
+        int soma = 0;
+
+        for(int i = 1; i <= 100; i += 2) {
+
+            soma += i;
+
+        }
+
+        System.out.println(soma);
+    }
+}
