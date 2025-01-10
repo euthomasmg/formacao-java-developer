686a7405899403ec4fab96b33b8990b18d00d98d
Thomas <thomas2003.mayer@gmail.com>
2025-01-10 20:08:38 -0300

    commit

---
+++ escopo-estrutura-java/estruturas-de-repeticao/src/SomaArray.java
@@ -0,0 +1,14 @@
+public class SomaArray {
+
+    public static void main(String[] args){
+
+        int numeros[] = {23, 67, 43, 90};
+
+        for (int i = 0; i <= numeros.length; i++){
+
+            System.out.println(numeros[i]);
+
+            int resultado =
+        }
+    }
+}
